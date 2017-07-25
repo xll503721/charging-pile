3672bddfda53ab4cc2bae25a821ee27952237951
lizhixuan1 <lizhixuan1@yy.com>
2017-07-25 11:27:30 +0800

    添加mocha test debug

---
+++ test/pile.test.js
@@ -0,0 +1,18 @@
+describe("测试记录服务的方法", function() {
+
+    it("插入一个记录", function() {
+        
+    })
+    
+    it("查询一个记录", function() {
+        
+    })
+
+    it("更新一个记录", function() {
+        
+    })
+
+    it("删除一个记录", function() {
+        
+    })
+});
